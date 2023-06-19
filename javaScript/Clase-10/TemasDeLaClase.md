489e06350514900a590b2b3a26bdcbf959b7cc15
Pablo Vicente <pablovicente19@hotmail.com>
2023-06-19 13:27:37 -0300

    termine clase 10

---
+++ javaScript/Clase-10/TemasDeLaClase.md
@@ -0,0 +1,6 @@
+### Temas de la clase 10:
+  - DOM
+  - Nodos
+  - Metodos del DOM
+  - windowm, document, element
+  - funcionalidades del browser(timer, networking, location, userAgent)
---
+++ javaScript/Clase-10/clase-10.js
@@ -0,0 +1,11 @@
+// Que es el DOM:
+/*
+  El dom es lo que permite interactuar a JS con los elementos HTML
+*/
+
+// Metodos basicos del DOM:
+/*
+  - getElementById()
+  - getElementByClassName()
+  - getElementByTagName()
+*/
