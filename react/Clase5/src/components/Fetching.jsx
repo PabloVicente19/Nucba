2ee0504b60c70f98c548be80ea2ca3d568fdb0d6
Pablo Vicente <pablovicente19@hotmail.com>
2023-06-25 12:46:12 -0300

    terminar clase 5 React

--- react/Clase5/src/components/Fetching.jsx
+++ react/Clase5/src/components/Fetching.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 
+const postId =[1,2,3,4,5,6,7]
+// Hago el fetch a jsonPlaceholder
+const fetchPost = async (id) => {
+  const apiCall  = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const respuesta = await apiCall.json()
+  return console.log(respuesta);
+}
 
 export default function Fetching() {
+  const [index, setIndex] = useState(0)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const [post, setPost] = useState(null)
+
+  
   return (
     <div>Fetching</div>
   )
